79fb1fe34ea255805cfb6d70b563d3d674962c61
John Smith <example@standford.edu>
2013-07-11 14:16:22 +0000

    Added lines2 in web.js

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-//var express = require('express');
-var fs require('fs');
+var express = require('express');
+var fs = require('fs');
 
 fs.readFile('/index.html', function(err, data)
 {
 if(err) throw err;
 console.log(data);
 });
-//var app = express.createServer(express.logger());
+var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-//});
+app.get('/', function(request, response) {
+  response.send(data);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
