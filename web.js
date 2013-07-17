f066e761db0a47a270656303aa4f5f4e717951ff
John Smith <example@standford.edu>
2013-07-17 17:30:59 +0000

    Changed port11  on web.js

--- web.js
+++ web.js
@@ -6,11 +6,13 @@ var home = fs.readFileSync("index.html", "utf-8");
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
   response.send(home);
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
